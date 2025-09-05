26ea8ea12f21c29524058e61e9da8d0a069d09e6
dlkingery <don.kingery@education.ohio.gov>
2025-09-05 17:20:15 -0400

    Update streamlit_app.py
    Upper case table name FRUIT_NAME

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ cnx=st.connection("snowflake")
 #session = get_active_session()
 session = cnx.session
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect(
