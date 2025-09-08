6d744a486443d1cbde57b7f62d7c315916ddfc01
dlkingery <don.kingery@education.ohio.gov>
2025-09-08 11:46:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,19 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title('My Parents New Healthy Diner')
 st.write(
-  """Breakfast Menu"""
+  "Breakfast Menu"
 )
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be: ", name_on_order)
 
 cnx=st.connection("snowflake")
-#session = get_active_session()
 session = cnx.session
 
-# my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 
 ingredients_list=st.multiselect(
